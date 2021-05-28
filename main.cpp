437a4b7139439dc9dcdb72fd149fc916062a8a01
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-05-28 15:18:32 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+	int numToGuess, guessedNum, generatedRandomNumber;
+	string result;
+	bool gameLoop = true;
+
+	while (gameLoop) {
+		char playAgain;
+		srand(time(NULL));
+		cout << "Number to guess: \n";
+		cin >> numToGuess;
+	
+		system("CLS");
+
+		generatedRandomNumber = int( ( rand() % 100 + 1 ) / 10 );
+
+		cout << "Hint: The number is between " << numToGuess - generatedRandomNumber << " and " 
+			<< (10 - generatedRandomNumber) + numToGuess << "\n";
+
+		cout << "Your guess: ";
+		cin >> guessedNum;
+
+		result = (guessedNum == numToGuess) ? "You won!" : "You lost!";
+
+		cout << result << "\n";
+
+		cout << "Play again? (y/n) ";
+		cin >> playAgain;
+		
+		if (tolower(playAgain) == 'n') gameLoop = false;
+		else system("CLS");
+	}
+}
